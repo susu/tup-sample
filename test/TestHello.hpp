e7db280c7630967be775d857314f863adf7fa29b
Marton Suranyi <marton.suranyi@gmail.com>
2012-04-23 14:42:35 +0200

    cxxtest added

--- test/TestHello.hpp
+++ test/TestHello.hpp
@@ -1,13 +1,13 @@
 #include <cxxtest/TestSuite.h>
 
-#include "hello.hpp"
+#include "hello/hello.hpp"
 
 class TestHello : public CxxTest::TestSuite
 {
   public:
     void test_Hello_should_be_created_via_ctor()
     {
-      Hello h;
+      hello::Hello h;
     }
 };
 
